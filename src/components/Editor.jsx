01a5a04fee3f81f48d9feeae624ba81c79a13f18
NguyenAnh2003 <cunho2803032003@gmail.com>
2023-11-08 22:23:38 +0700

    #code

--- src/components/Editor.jsx
+++ src/components/Editor.jsx
@@ -172,15 +172,43 @@ const Editor = React.memo(({ pageId }) => {
         <b className="">Page name: </b>
         {data.name}
       </p>
-      <div className='flex flex-row justify-between pl-3 pr-3 pb-4'>
+      <div className="flex flex-row justify-between pl-3 pr-3 pb-4">
         <div> Users joined: {group && group.map((i) => <UserCard key={i.socketId} name={i.name} socketId={i.socketId} />)}</div>
         {/* textfield search for users to add to pageId */}
-        <div>
-          <input ref={searchUserRef} placeholder="Search to add" />
-          <button onClick={searchHandler}>Search</button>
+        <div className="flex flex-col">
+          <div>
+            <form className="flex items-center">
+              <div className="w-full">
+                <input
+                  type="text"
+                  ref={searchUserRef}
+                  id="simple-search"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  placeholder="Search branch name..."
+                  required
+                />
+              </div>
+              <button
+                type="button"
+                onClick={searchHandler}
+                className="p-2.5 ml-2 text-sm font-medium text-white bg-black rounded-lg border border-black focus:outline-none "
+              >
+                <svg className="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                  <path
+                    stroke="currentColor"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
+                  />
+                </svg>
+                <span className="sr-only">Search</span>
+              </button>
+            </form>
+          </div>
+          {userSearched ? <UserSearchedCard pageId={pageId} userId={userSearched.userId} username={userSearched.username} /> : <></>}
         </div>
       </div>
-      {userSearched ? <UserSearchedCard pageId={pageId} userId={userSearched.userId} username={userSearched.username} /> : <></>}
       {/* React Quill Editor */}
       <ReactQuill
         theme="snow"
