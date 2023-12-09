03e847b18331980021ae9604fec1ff40fabd8acf
NguyenAnh2003 <cunho2803032003@gmail.com>
2023-12-09 11:08:18 +0700

    #code

--- src/config/quill.config.js
+++ src/config/quill.config.js
@@ -1,9 +1,4 @@
 /* eslint-disable no-unused-vars */
-import { ImageHandler, VideoHandler, AttachmentHandler } from 'quill-upload';
-import { Quill } from 'react-quill';
-import axiosConfig from './api.config';
-// Quill.register('modules/imageHandler', ImageHandler);
-
 export const module = {
   toolbar: [
     [{ header: '1' }, { header: '2' }, { font: [] }],
@@ -13,28 +8,6 @@ export const module = {
     ['link', 'image', 'video'],
     ['clean'],
   ],
-  // imageHandler: {
-  //   upload: (file) => {
-  //     return new Promise((resolve, reject) => {
-  //       console.log(file);
-  //       const formData = new FormData();
-  //       formData.append('file', file);
-  //       axiosConfig.post('/upload', formData).then(
-  //         ({ data, status }) => {
-  //           if (status === 200) {
-  //             setTimeout(() => {
-  //               console.log(data);
-  //               resolve(data);
-  //             }, 3000);
-  //           }
-  //         },
-  //         (err) => {
-  //           reject(err);
-  //         }
-  //       );
-  //     });
-  //   },
-  // },
   clipboard: {
     // toggle to add extra line breaks when pasting HTML:
     matchVisual: false,
